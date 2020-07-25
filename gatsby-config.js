7b0e32bd8e8e26c10c195b6a80d05e83f9eeabf5
Trevor Kilvington <trevorgk@gmail.com>
2020-07-26 07:49:54 +1000

    Metadata etc

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: 'Gatsby + Netlify CMS Starter',
+    title: "Grant Kilvington's Website",
     description:
-      'This repo contains an example business website that is built with Gatsby, and Netlify CMS.It follows the JAMstack architecture by using Git as a single source of truth, and Netlify for continuous deployment, and CDN distribution.',
+      'This website belongs to Grant Kilvington and contains various recipes, anecdotes, bridge hands and funny cartoons. It is built using Gatsby + Netlify CMS Starter',
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -74,4 +74,4 @@ module.exports = {
     }, // must be after other CSS plugins
     'gatsby-plugin-netlify', // make sure to keep it last in the array
   ],
-}
+};
