81aca4aa3300904e6a5b02703b6e0f4052f50f76
Trevor Kilvington <trevorgk@gmail.com>
2020-09-11 22:41:49 +1000

    Add aside

--- src/templates/humour.js
+++ src/templates/humour.js
@@ -3,21 +3,26 @@ import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import Layout from '../components/Layout';
 import Content, { HTMLContent } from '../components/Content';
+import HumourAside from '../components/HumourAside';
 
 export const HumourPageTemplate = ({ title, content, contentComponent }) => {
   const PageContent = contentComponent || Content;
 
   return (
     <section className="section section--gradient">
-      <div className="container">
+      <div className="container content">
         <div className="columns">
-          <div className="column is-10 is-offset-1">
+          <div className="column is-three-quarters">
             <div className="section">
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                 {title}
               </h2>
               <PageContent className="content" content={content} />
             </div>
+          </div>
+
+          <div className="column">
+            <HumourAside />
           </div>
         </div>
       </div>
