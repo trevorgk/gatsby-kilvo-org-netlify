ab30b087f760469105e3aef177796ddd699dbe2b
Trevor Kilvington <trevorgk@gmail.com>
2020-09-14 06:45:39 +1000

    Add logging to header script

--- src/utils/getMinifiedScript.js
+++ src/utils/getMinifiedScript.js
@@ -1,10 +1,12 @@
 export const externaliseLinksScript = `
+console.log('in head');
 document.addEventListener('DOMContentLoaded', function externaliseLinks() {
   var links = document.links;
+  console.log('DOMContentLoaded', {links});
 
   for (var i = 0, linksLength = links.length; i < linksLength; i++) {
     if (links[i].hostname != window.location.hostname) links[i].target = '_blank';
   }
-})`
-  .replace(/\n/g, '')
-  .replace(/\s+/g, ' ');
+})`;
+//.replace(/\n/g, '')
+//.replace(/\s+/g, ' ');
