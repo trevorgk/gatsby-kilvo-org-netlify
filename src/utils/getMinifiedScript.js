86076cade9c9a7633ea2dbbd6a48c0e2c5d4ab2f
Trevor Kilvington <trevorgk@gmail.com>
2020-09-20 07:01:03 +1000

    Commit script

--- src/utils/getMinifiedScript.js
+++ src/utils/getMinifiedScript.js
@@ -1,8 +1,7 @@
 export const externaliseLinksScript = `
 document.addEventListener('DOMContentLoaded', function externaliseLinks() {
   var links = document.links;
-  console.log('DOMContentLoaded', {links});
-
+  
   for (var i = 0, linksLength = links.length; i < linksLength; i++) {
     if (links[i].hostname != window.location.hostname) links[i].target = '_blank';
   }
